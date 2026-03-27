8ba7451e546ee709094e80c2628bc956983ab9d6
Krupal Mansata <krupal@ciphercraft.co>
2026-03-27 15:28:43 +0530

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,6 +1,17 @@
-// src/i18n.ts
 import {getRequestConfig} from 'next-intl/server';
+import {routing} from './routing';
  
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`../../messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({requestLocale}) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale;
+ 
+  // Ensure that a valid locale is used
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
+ 
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default
+  };
+});
